2d17d5243241f05d12c5f0836e24bca7987b0c71
Thinh Vu <thinh@bitski.com>
2023-07-26 11:35:23 -0400

    Make threshold configurable

--- contracts/samples/gnosis/GnosisAccountFactory.sol
+++ contracts/samples/gnosis/GnosisAccountFactory.sol
@@ -20,8 +20,8 @@ contract GnosisSafeAccountFactory {
         eip4337Manager = _eip4337Manager;
     }
 
-    function createAccount(address owner,uint256 salt) public returns (address) {
-        address addr = getAddress(owner, salt);
+    function createAccount(address owner, uint threshold, uint256 salt) public returns (address) {
+        address addr = getAddress(owner, threshold, salt);
         uint codeSize = addr.code.length;
         if (codeSize > 0) {
             return addr;
@@ -30,10 +30,9 @@ contract GnosisSafeAccountFactory {
                 safeSingleton, getInitializer(owner), salt));
     }
 
-    function getInitializer(address owner) internal view returns (bytes memory) {
+    function getInitializer(address owner, uint threshold) internal view returns (bytes memory) {
         address[] memory owners = new address[](1);
         owners[0] = owner;
-        uint threshold = 1;
         address eip4337fallback = eip4337Manager.eip4337Fallback();
 
         bytes memory setup4337Modules = abi.encodeCall(
@@ -51,8 +50,8 @@ contract GnosisSafeAccountFactory {
      * calculate the counterfactual address of this account as it would be returned by createAccount()
      * (uses the same "create2 signature" used by SafeProxyFactory.createProxyWithNonce)
      */
-    function getAddress(address owner,uint256 salt) public view returns (address) {
-        bytes memory initializer = getInitializer(owner);
+    function getAddress(address owner, uint threshold, uint256 salt) public view returns (address) {
+        bytes memory initializer = getInitializer(owner, threshold);
         //copied from deployProxyWithNonce
         bytes32 salt2 = keccak256(abi.encodePacked(keccak256(initializer), salt));
         bytes memory deploymentData = abi.encodePacked(proxyFactory.proxyCreationCode(), uint256(uint160(safeSingleton)));
